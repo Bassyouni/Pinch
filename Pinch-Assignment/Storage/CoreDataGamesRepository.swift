aec50a2bb09a506613cbe3a86ea658b20c3bc479
Omar Ashraf El-Bassiony <omae.bassyouni991@gmail.com>
2024-10-23 16:46:55 +0200

    make loadingGames does nothing when store is empty

---
+++ Pinch-Assignment/Storage/CoreDataGamesRepository.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataGamesRepository.swift
+//  Pinch-Assignment
+//
+//  Created by Omar Bassyouni on 23/10/2024.
+//
+
+import Combine
+import CoreData
+
+public final class CoreDataGamesRepository {
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    public init(inMemory: Bool = false) {
+        let container = NSPersistentContainer(name: "GamesStore")
+        
+        if inMemory {
+            let description = NSPersistentStoreDescription()
+            description.type = NSInMemoryStoreType
+            container.persistentStoreDescriptions = [description]
+        }
+        
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load Core Data store: \(error)")
+            }
+        }
+        
+        self.container = container
+        self.context = container.newBackgroundContext()
+    }
+}
+
+extension CoreDataGamesRepository: GamesLoader {
+    public func loadGames() -> AnyPublisher<[Game], Error> {
+        return Just([])
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+}
---
+++ PinchAssignmentTests/Storage/CoreDataGamesCoreDataGamesRepositoryTests.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataGamesRepositoryTests.swift
+//  Pinch-Assignment
+//
+//  Created by Omar Bassyouni on 23/10/2024.
+//
+
+import XCTest
+import Combine
+import Pinch_Assignment
+
+final class CoreDataGamesRepositoryTests: XCTestCase {
+    var cancellables = Set<AnyCancellable>()
+    
+    func test_loadGames_hasNoSideEffectsOnEmptyRepository() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for cache retrieval")
+        var receivedGames: [Game]?
+        
+        sut.loadGames()
+            .sink { result in
+                switch result {
+                case .finished:
+                    XCTAssertEqual(receivedGames, [])
+                default:
+                    XCTFail("Expected to receive no elements successfully")
+                }
+                
+                exp.fulfill()
+            } receiveValue: { games in
+                receivedGames = games
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
+
+private extension CoreDataGamesRepositoryTests {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataGamesRepository {
+        let sut = CoreDataGamesRepository(inMemory: true)
+        checkForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+}
