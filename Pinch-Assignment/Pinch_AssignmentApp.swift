ce259976dedadc7962209275ff4d6ca473fb0e23
Omar Ashraf El-Bassiony <omae.bassyouni991@gmail.com>
2024-10-23 22:48:08 +0200

    use `AppNavigationView` rather than `NavigationViewI' nstead

--- Pinch-Assignment/Pinch_AssignmentApp.swift
+++ Pinch-Assignment/Pinch_AssignmentApp.swift
@@ -11,17 +11,33 @@ import SwiftUI
 struct Pinch_AssignmentApp: App {
     var body: some Scene {
         WindowGroup {
-            let url = URL(string: "https://api.igdb.com/v4/games")!
-            let clientId = "ctgyj1u5eoe8ynxsoi0anhpctz1oo6"
-            let bearerToken = "iawmqtbgk5h47jjglcn4v7sofkue9v"
-            let client = URLSessionHTTPClient()
-            let remoteLoader = RemoteGamesLoader(url: url, clientID: clientId, bearerToken: bearerToken, client: client)
-            let store = CoreDataGamesStore()
-            let gamesLoader = LocalWithRemoteFallbackGamesLoader(store: store, remote: remoteLoader)
-            
-            NavigationView {
-                GamesListView(viewModel: GameListViewModel(gamesLoader: MainQueueDispatchDecorator(gamesLoader)))
+            AppNavigationView { view, _ in
+                switch view {
+                case .gameList:
+                    makeGameListView()
+                    
+                case .gameDetails:
+                    makeGameDetailsView()
+                }
             }
         }
+    }
+    
+    private func makeGameListView() -> some View {
+        let url = URL(string: "https://api.igdb.com/v4/games")!
+        let clientId = "ctgyj1u5eoe8ynxsoi0anhpctz1oo6"
+        let bearerToken = "iawmqtbgk5h47jjglcn4v7sofkue9v"
+        let client = URLSessionHTTPClient()
+        let remoteLoader = RemoteGamesLoader(url: url, clientID: clientId, bearerToken: bearerToken, client: client)
+        let store = CoreDataGamesStore()
+        let gamesLoader = LocalWithRemoteFallbackGamesLoader(store: store, remote: remoteLoader)
+        let viewModel = GameListViewModel(gamesLoader: MainQueueDispatchDecorator(gamesLoader))
+        
+        return GamesListView(viewModel: viewModel)
+    }
+    
+    @ViewBuilder
+    private func makeGameDetailsView() -> some View {
+        Text("Game Details")
     }
 }
